18e3793507f74082637be2915c53ec72e3131eca
SelTarARI <seltarari@gmail.com>
2024-11-09 00:37:41 +0300

    artık çalışıyo azcık

--- raytracer.cpp
+++ raytracer.cpp
@@ -59,7 +59,7 @@ Vec3f raySphereIntersection(Ray ray, Vec3f center, float radius, bool& hit) {
         hit = false;
         return {0,0,0};
     }
-    float t = (-b - std::sqrt(discriminant)) / (2.0 * a);
+    float t = (-1.0*b - std::sqrt(discriminant)) / (2.0 * a);
     if (t < 0) {
         hit = false;
         return {0,0,0};
@@ -69,6 +69,43 @@ Vec3f raySphereIntersection(Ray ray, Vec3f center, float radius, bool& hit) {
     return intersection;
 }
 
+using namespace parser;
+Vec3f rayTriangleIntersection(Ray ray, Vec3f triA, Vec3f triB, Vec3f triC, bool& hit){
+    Vec3f edge1 = minus(triB, triA);
+    Vec3f edge2 = minus(triC, triA);
+    Vec3f h = cross(ray.direction, edge2);
+    float a = dot(edge1, h);
+    if (a > -0.00001 && a < 0.00001) {
+        hit = false;
+        return {0,0,0};
+    }
+    float f = 1.0 / a;
+    Vec3f s = minus(ray.origin, triA);
+    float u = f * dot(s, h);
+    if (u < 0 || u > 1) {
+        hit = false;
+        return {0,0,0};
+    }
+    Vec3f q = cross(s, edge1);
+    float v = f * dot(ray.direction, q);
+    if (v < 0 || u + v > 1) {
+        hit = false;
+        return {0,0,0};
+    }
+    float t = f * dot(edge2, q);
+    if (t > 0.00001) {
+        hit = true;
+        return plus(ray.origin, timesScalar(ray.direction, t));
+    }
+    hit = false;
+    return {0,0,0};
+}
+
+Vec3f rayMeshIntersection(Ray ray, Vec3f triA, Vec3f triB, Vec3f triC, bool& hit){
+    
+}
+
+/*
 using namespace parser;
 Vec3f rayTriangleIntersection(Ray ray, Vec3f triA, Vec3f triB, Vec3f triC, bool& hit){
     Vec3f edge1 = minus(triB, triA);
@@ -109,6 +146,7 @@ Vec3f rayTriangleIntersection(Ray ray, Vec3f triA, Vec3f triB, Vec3f triC, bool&
     hit = true;
     return p;
 }
+*/
 
 //using namespace parser;
 //Vec3f rayIntersection(Ray ray) {}
@@ -158,31 +196,31 @@ int main(int argc, char* argv[])
         {
             for (int x = 0; x < width; ++x)
             {
-                bool hit=false;
+                bool hit1=false;
 
                 Ray ray = generateRay(scene.cameras[0], x, y);
-                //printf("Ray Origin: %f %f %f\n", ray.origin.x, ray.origin.y, ray.origin.z);
-                //printf("Ray Direction: %f %f %f\n", ray.direction.x, ray.direction.y, ray.direction.z);
-                Vec3f coordinate = raySphereIntersection(ray, scene.vertex_data[scene.spheres[0].center_vertex_id], scene.spheres[0].radius, hit);
-                //Vec3f triA = scene.vertex_data[scene.meshes[0].faces[0].v0_id];
-                //Vec3f triB = scene.vertex_data[scene.meshes[0].faces[0].v1_id];
-                //Vec3f triC = scene.vertex_data[scene.meshes[0].faces[0].v2_id];
-                //Vec3f coordinateTri = rayTriangleIntersection(ray, triA, triB, triC, hit);
+                Vec3f center = scene.vertex_data[scene.spheres[0].center_vertex_id-1];
+                float radius = scene.spheres[0].radius;
+                Vec3f coordinate = raySphereIntersection(ray, center, radius, hit1);
 
-                if (hit) { //red
+                bool hit=false;
+                Vec3f v0=scene.vertex_data[scene.triangles[0].indices.v0_id-1];
+                Vec3f v1=scene.vertex_data[scene.triangles[0].indices.v1_id-1];
+                Vec3f v2=scene.vertex_data[scene.triangles[0].indices.v2_id-1];
+                Vec3f coordinateTri = rayTriangleIntersection(ray, v0, v1, v2, hit);
+
+                if (hit || hit1) {
                     printf("Hit\n");
                     image[i++] = 255;
+                    image[i++] = 237;
                     image[i++] = 0;
+                } else {
                     image[i++] = 0;
-                } else { //blue
-                    image[i++] = 0;
-                    image[i++] = 0;
-                    image[i++] = 255;
+                    image[i++] = 45;
+                    image[i++] = 114;
                 }
             }
         }
-        write_ppm("test.ppm", image, width, height);
+        write_ppm(scene.cameras[j].image_name.c_str(), image, width, height);
     }
-
-
 }
